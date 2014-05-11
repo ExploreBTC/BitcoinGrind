bc3c728c324b919957fadab570e4a7e8fa369f47
ExploreBTC <explorebtc@gmail.com>
2014-05-11 13:52:05 -0400

    Update g.js

--- g.js
+++ g.js
@@ -2,17 +2,10 @@ var s=[
 "gin.htm",
 "http://freebitco.in/?r=135139",
 "http://dailybitcoins.org/index.php?aff=9fb5ef25df8f3dc11cbf6a7c967d235d",
-"http://bitcoinfaucet.tk/?ref=1McYLx1YbnUZ5fRPw5Y24PBBQVutf89U6m",
-"http://www.bitcoins4.me/",
-"http://bitcoiner.net/",
-"http://www.btcflow.net/",
 "http://hotswap.co/?id=369693",
-"http://mmoclub.com/faucet",
-"http://bithunter.info/?id=28050",
-"http://btcfaucet.net/?id=92810",
-"http://www.minifrij.pw",
-"http://megabitcoinfaucet.com/?id=2227",
+"http://www.minifrij.pw?ref=1McYLx1YbnUZ5fRPw5Y24PBBQVutf89U6m",
 "http://www.8coin.me/?r=7T8dOw&a=",
+"http://www.getbitcoinsquick.com/?r=7T8dOw&a=",
 "http://www.bbitcoin.me/?r=7T8dOw&a=",
 "http://www.bitcoinfaucet.me/?r=7T8dOw&a=",
 "http://www.bitcurl.me/?r=7T8dOw&a=",
@@ -25,9 +18,11 @@ var s=[
 "http://www.freebitcoin.mobi/?r=7T8dOw&a=",
 "http://www.freebitcoin.ws/?r=7T8dOw&a=",
 "http://www.freecoinbit.com/?r=7T8dOw&a=",
-"http://www.getbitcoinsquick.com/?r=7T8dOw&a=",
 "http://www.raincoins.net/?r=7T8dOw&a=",
 "http://www.smurfcoin.com/?r=7T8dOw&a=",
+"http://www.bitcoins4.me/",
+"http://bitcoiner.net/",
+"http://www.btcflow.net/",
 "http://www.freebitcoins.me/?a=",
 "http://www.thefreebitcoins.com/?a=",
 "http://www.srbitcoin.com/?a=",
@@ -49,14 +44,9 @@ var s=[
 "http://www.freebitcoins4u.com/?a=",
 "http://www.freebitcoinlottery.com/?a=",
 "http://multifauceted.com",
-"http://foxfaucet.com",
 "http://www.cleverpuffin.com/faucet/?id=19158",
 "http://www.rabbitcoin.tk",
 "http://www.freakfaucet.com",
-"http://www.hotbitcoins.me",
-"http://www.hotbitcoin.me",
-"http://nubtektrix.us",
-"http://bitcoinblizzard.com/faucetsplash.php",
 "http://superfaucet.com",
 "http://www.megafaucet.com",
 "http://grifobitcoin.com",
@@ -64,7 +54,6 @@ var s=[
 "http://bitcoinhahn.de",
 "http://faucet.galat.org",
 "http://bitcoinflood.com/faucet/",
-"http://bitfirefaucet.com",
 "http://www.bitcrate.net/index.php?ref=3665"
 ];
 var adr,i,x=0,c=s.length;
@@ -76,17 +65,17 @@ s=s.map(function(x){
 }
 function next(){
 x+=1;
-if (x>c)
+if (x>c-1)
   {
-  x=1;
+  x=0;
   }
 changeSrc();
 }
 function prev(){
 x-=1;
-if (x<1)
+if (x<=0)
   {
-  x=c;
+  x=c-1;
   }
 changeSrc();
 }
@@ -94,7 +83,7 @@ function jumpTo() {
 i=prompt("Enter a number to skip to that faucet.");
 x=0;
 while (i!=x) {
-  if (x>c) {
+  if (x>c-2) {
   break;
   }
   x+=1;
